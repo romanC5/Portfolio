77f6bd8db927f5e09e9791055b399c052839b247
romanC5 <romandangol91@gmail.com>
2026-07-20 16:23:29 +0545

    home page figma export

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',  {
+    strings: ['Frontend Developer', 'Student', 'Web Developer', 'Gamer', 'Cybersecurity Enthusiast'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+ });
